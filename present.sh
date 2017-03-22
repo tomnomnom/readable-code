106ecb10b920288f9d13232f0909af22ae4a90f6
Tom Hudson <mail@tomnomnom.com>
2017-03-22 12:44:54 +0000

    Adds dark option to present script

--- present.sh
+++ present.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-VIMENV=talk vim $(find . -maxdepth 1 -name "[0-9][0-9]*" | sort)
+if [ "$1" == "dark" ]; then
+    export VIMENV=
+else
+    export VIMENV=talk
+fi
+
+vim $(find . -maxdepth 1 -name "[0-9][0-9]*" | sort)
